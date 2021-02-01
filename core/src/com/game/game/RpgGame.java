1939a91698b67beddff21528eee34a9aa0f3ded2
IMBABOT1 <zlovred12@gmail.com>
2021-02-01 20:39:03 +0300

    temp

--- core/src/com/game/game/RpgGame.java
+++ core/src/com/game/game/RpgGame.java
@@ -1,33 +1,31 @@
-package com.game.game;
+package com.rogue.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class RpgGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+public class RpgGame extends Game {
+    private SpriteBatch batch;
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        ScreenManager.getInstance().init(this, batch);
+        ScreenManager.getInstance().switchScreen(ScreenManager.ScreenType.MENU);
+    }
+
+    @Override
+    public void render() {
+        float dt = Gdx.graphics.getDeltaTime();
+        getScreen().render(dt);
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        ScreenManager.getInstance().dispose();
+    }
 }
